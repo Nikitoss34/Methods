6fccbd8852b08d068b895af7067652e387a5ba16
Mykyta <honcharov.nikita70@gmail.com>
2023-11-09 13:18:19 +0100

    first commit

---
+++ .idea/Src/Methods.java
@@ -0,0 +1,38 @@
+public class Methods {
+    public static void main(String[] args) {
+        //String name = "ABBA";
+        // int age = 21;
+        //play(name, age);
+
+
+        //}
+
+        //static void play(String name, int age) {
+        //System.out.println(" play " + name);
+        //System.out.println(" I am " + age);
+
+        //}
+        //int x = 2;
+        //int x2 = 5;
+        //int x3 = 10;
+
+        //int a = add(x, x2, x3);
+        //System.out.println(a);
+        // }
+        //static int add(int x, int x2, int x3) {
+        //int a = x + x2 + x3;
+        //return a;
+
+    double x = add(1, 2, 3);
+    System.out.println(x);
+}
+        static int add( int a, int b, int c){
+            System.out.println("This is first method");
+            return a+b+c;
+        }
+        static int add(int a, int b, int c, int d){
+           System.out.println("This is second method");
+           return a+b+c+d;
+        }
+    }
+
